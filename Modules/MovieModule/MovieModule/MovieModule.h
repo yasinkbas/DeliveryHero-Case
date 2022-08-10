4e2e488abf5f7040c4fded152e6aae9bdcce8b44
Yasin Akbas <yasin.kbas12@gmail.com>
2022-08-10 21:08:09 +0300

    initialize movieModule

---
+++ Modules/MovieModule/MovieModule/MovieModule.h
@@ -0,0 +1,18 @@
+//
+//  MovieModule.h
+//  MovieModule
+//
+//  Created by Yasin Akbas on 10.08.2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MovieModule.
+FOUNDATION_EXPORT double MovieModuleVersionNumber;
+
+//! Project version string for MovieModule.
+FOUNDATION_EXPORT const unsigned char MovieModuleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MovieModule/PublicHeader.h>
+
+
